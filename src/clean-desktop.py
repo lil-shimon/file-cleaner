e2834821a10615c6b4222ff593fa7fc3ca7af867
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-02-12 01:41:51 +0900

    feat: 🎸 テストを作成

---
+++ src/clean-desktop.py
@@ -0,0 +1,6 @@
+def clean_desktop_handler():
+    print("clean desktop")
+
+
+if __name__ == "__main__":
+    clean_desktop_handler()
---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "テストを実行します >>> "
+python src/clean-desktop.py
