f3d68fc12f8d1135a25e0a5b8d96ded4c402d271
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-02-12 02:01:24 +0900

    feat: 🎸 初めての実行の場合、バックアップフォルダを作成する処理を追加

--- src/clean-desktop.py
+++ src/clean-desktop.py
@@ -1,5 +1,39 @@
+import os
+from os.path import expanduser  # ホームディレクトリ取得のため
+
+
+def get_homedir():
+    """
+    ホームディレクトリを取得する関数
+    :return: string
+    """
+    return expanduser("~")
+
+
+def check_mkdirs(path):
+    """
+    フォルダが存在していなかったら作成する関数
+    :param path: string
+    :return:
+    """
+
+    # もしフォルダがなかったら作成する
+    if not os.path.isdir(path):
+        print('初めての実行ですね。デスクトップのファイルをBackUpするためにバックアップフォルダを作成しました')
+        os.makedirs(path)
+
+
 def clean_desktop_handler():
-    print("clean desktop")
+    """
+    デスクトップをきれいにする関数
+
+    :return: void
+    """
+
+    home_dir = get_homedir()  # ホームディレクトリを取得
+    path_to_backup = home_dir + "/desktop-backups"  # デスクトップファイルPath
+
+    check_mkdirs(path_to_backup)
 
 
 if __name__ == "__main__":
