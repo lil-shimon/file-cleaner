bc0a4a6be28ec79c11b3889444eb6c233d326a08
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-02-14 17:58:22 +0900

    feat: 🎸 Downloadフォルダの掃除を実装

--- src/clean-desktop.py
+++ src/clean-desktop.py
@@ -24,7 +24,11 @@ def check_mkdirs(path, message=None):
 
     # もしフォルダがなかったら作成する
     if not os.path.isdir(path):
-        os.makedirs(path)
+        print("path", path)
+        try:
+            os.makedirs(path)
+        except FileExistsError:
+            return
         if message:
             print(message)
 
@@ -58,7 +62,11 @@ def move_to_backup(files, path):
             return
         backup_path_by_extension = path + "/" + ext  # ファイルごとのバックアップフォルダ
         check_mkdirs(backup_path_by_extension, "新しい拡張子が掃除対象になりました。フォルダを作成します")  # 拡張子のバックアップフォルダが存在していなかったら作成する
-        shutil.move(file, backup_path_by_extension)
+        try:
+            shutil.move(file, backup_path_by_extension)
+        except shutil.Error:
+            print(file, "が移動できませんでした")
+
         print(file + "が移動完了しました。。。。")
 
 
@@ -103,6 +111,7 @@ def clean_backup_handler():
     backup_files = get_file_info_from_folder(backup_path + "/*")  # backup files
 
     if not backup_files:
+        print("バックアップフォルダはとてもきれいです！")
         return
     if backup_files:
         files = [f for f in backup_files if os.path.isfile(os.path.join(backup_path, f))]
@@ -116,15 +125,59 @@ def clean_backup_handler():
             print(file, "を移動しました")
 
 
+def clean_download_handler():
+    """
+    ダウンロードフォルダをきれいにする関数
+    :return:
+    """
+
+    root_path = get_dir_path("~")
+    download_path = os.path.join(root_path, "Downloads")  # download path
+    download_files = get_file_info_from_folder(download_path + "/*")  # download files
+
+    print("ダウンロードフォルダ用のバックアップを作成しますか？ y/n [default: no]")
+    cmd = input()
+    if cmd == 'y' or cmd == "yes":
+        print("この処理には時間がかかる可能性があります...")
+        download_backup_path = root_path + "/download-backups"
+        check_mkdirs(download_backup_path, "ダウンロード用のバックアップフォルダを作成しました。")
+        try:
+            shutil.rmtree(download_backup_path)  # if there is download backup folder, delete it.
+            shutil.copytree(download_path, download_backup_path)  # copy download folder into download backup folder
+            print("バックアップを作成しました")
+        except FileExistsError:  # just in case
+            return
+
+    if not download_files:
+        print("ダウンロードフォルダはとてもきれいです！")
+        return
+
+    files = [f for f in download_files if os.path.isfile(os.path.join(download_path, f))]  # check file exists
+
+    for file in files:
+        ext = get_file_ext(file)  # get extension
+        if not ext:  # if folder, skip
+            continue
+        download_path_by_ext = download_path + "/" + ext  # full path (include extension data)
+        check_mkdirs(download_path_by_ext, "新しい拡張子なので、フォルダを作成します")  # check folder by extension exists
+        shutil.move(file, download_path_by_ext)  # move file
+        print(file, "を移動しました")
+
+    print("掃除が終わりました!")
+
+
 def clean_handler():
     print('したい動作を選んでください')
     print('1. デスクトップをきれいにしたい [default]')
     print('2. バックアップフォルダを整理したい')
+    print('3. ダウンロードフォルダを整理したい')
     cmd = input()
     if not cmd or int(cmd) == 1:
         clean_desktop_handler()
     if int(cmd) == 2:
         clean_backup_handler()
+    if int(cmd) == 3:
+        clean_download_handler()
     else:
         return
 
